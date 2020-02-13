b9ebac2df75c8d940773041013941ed3a32e498e
cloudwindy <49033775+cloudwindy@users.noreply.github.com>
2020-02-13 18:04:16 +0800

    如你所愿 我的commit

--- main.py
+++ main.py
@@ -4,36 +4,66 @@
 # Author: Xiao_Jin
 
 import time
-from ping import auto_tcping, httping
+import ping
 from logzero import logger
 
 # 设置
-VERSION = 'DEV'
+VERSION = '1.0.01'
 SERVER_LIST = [{
     'host': 's0.ctuin.xyz',
-    'name': 'SkEy-Serv'
+    'name': 'SkEy-Serv',
+    'up': True
 },
 {
     'host': 's1.ctuin.xyz',
-    'name': 'SkEy-S1' 
+    'name': 'SkEy-S1',
+    'up': True
 },
 {
     'host': 's2.ctuin.xyz',
-    'name': 'SkEy-S2'
+    'name': 'SkEy-S2',
+    'up': True
 }]
 PORT = 443
 ICMPING_NUM = 4
 DELAY_TIME = 5
+SKIP_TIME = 4
 
 logger.info('Batch-Healer - %s 已启动' % VERSION)
 
+def init():
+    for server in SERVER_LIST:
+        server['up'] = True
+        server['skip'] = 0
+
+def main():
+    while True:
+        for server in SERVER_LIST:
+            if server['up'] == True:
+                runping(server)
+            else:
+                if server['skip'] >= SKIP_TIME:
+                    server['skip'] = 0
+                    runping(server)
+                else:
+                    server['skip'] += 1
+        delay(DELAY_TIME)
+
+def runping(server):
+    logger.info('已选中 %s(%s)' % (server['name'], server['host']))
+    if ping.auto_tcping(server['host'], PORT):
+        server['up'] = False
+        logger.info('%s 已被标记为: 下线' % server['name'])
+    else:
+        if server['up'] == True:
+            logger.info('%s 已上线' % server['name'])
+        server['up'] = False
+        logger.info('%s 已被标记为: 在线' % server['name'])
+
 def delay(delay_time):
-    logger.debug('下次运行: %f 分钟后' % delay_time)
+    logger.info('下次运行: %d 分钟后' % delay_time)
     time.sleep(delay_time * 60)
 
-while True:
-    for server in SERVER_LIST:
-        logger.info('已选中 %s(%s)' % (server['name'], server['host']))
-        if auto_tcping(server['host'], PORT):
-            logger.error('该服务器已被标记为: 不通')
-    delay(DELAY_TIME)
+if __name__ == "__main__":
+    init()
+    main()
--- ping.py
+++ ping.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 # ======== Ctuin ========
 # Author: Xiao_Jin
@@ -7,32 +7,39 @@ import time
 import socket
 from logzero import logger
 
-MAXTRY = 4
-def auto_ping(ip, port):
+TCPING_MAX_TRY = 4
+TLSPING_PATH = '/health-check'
+
+def auto_tcping(ip, port):
     tried = 0
-    while tried < MAXTRY:
+    while tried < TCPING_MAX_TRY:
         if tcping(ip, port) == False:
             return False
         tried += 1
-    logger.error("连续 %d 次 TCPing 失败")
+    logger.error("连续 %d 次 TCPing 失败" % tried)
     return True
 
 def tcping(ip, port):
     logger.info('正在运行TCPing...')
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logger.debug('Socket 已初始化')
     try:
         time_start = time.time()  # 计时开始
-        s.connect((host, port))
-        s.send(b' ')
+        logger.debug('正在建立 TCP 连接')
+        s.connect((ip, port))
+        logger.debug('连接正常 正在发送 GET 字符串')
+        s.send(b'GET')
+        logger.debug('一切正常 测试结束')
         time_end = time.time()  # 计时结束
         s.close()
         logger.info('TCPing正常: 耗时%sms' % int(round(time_end - time_start, 3) * 1000))
         return False
-    except:
+    except Exception as e:
         time_end = time.time()  # 计时结束
+        logger.debug('出现错误: %s' % repr(e))
         s.close()
         logger.warning('TCPing失败: 耗时%sms' % int(round(time_end - time_start, 3) * 1000))
         return True
 
-def httping(host):
-    logger.info('正在运行HTTPing...')
+def tlsping(host):
+    logger.info('正在运行TLSPing...')
